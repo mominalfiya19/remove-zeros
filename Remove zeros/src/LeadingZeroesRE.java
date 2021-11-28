31ba43556b4afb859425a4b6fe3df65a463f0e1e
mominalfiya19 <91407480+mominalfiya19@users.noreply.github.com>
2021-11-28 20:33:24 +0530

    Add files via upload

---
+++ Remove zeros/src/LeadingZeroesRE.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class LeadingZeroesRE {
+   public static String removeLeadingZeroes(String str) {
+      String strPattern = "^0+(?!$)";
+      str = str.replaceAll(strPattern, "");
+      return str;
+   }
+   public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter an integer: ");
+      String num = sc.next();
+      String result = LeadingZeroesRE.removeLeadingZeroes(num);
+      System.out.println(result);
+   }
+}
